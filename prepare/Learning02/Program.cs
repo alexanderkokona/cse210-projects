f4503e44006f15949e6dff17ea336f9426d3d443
alexanderkokona <alexanderkokona@gmail.com>
2025-09-24 20:43:31 -0600

    learning 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Cybersecurity Analyst";
+        job1._company = "Idaho National Labratories (INL)";
+        job1._startYear = 2026;
+        job1._endYear = 2036;
+
+        Job job2 = new Job();
+        job2._jobTitle = "CISO";
+        job2._company = "Microsoft";
+        job2._startYear = 2036;
+        job2._endYear = 2046;
+
+        Resume myResume = new Resume();
+        myResume._name = "Alex Kokona";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
