e5d8d5ad43a403708111bcb676a86d6fa55bd08e
alexanderkokona <alexanderkokona@gmail.com>
2025-10-15 19:54:31 -0600

    Learning 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>();
+
+        // Add different shapes
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 3));
+        shapes.Add(new Circle("Green", 2.5));
+
+        // Display color and area for each shape
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea():F2}");
+            Console.WriteLine();
+        }
     }
 }
